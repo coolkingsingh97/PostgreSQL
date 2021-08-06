54b056cbe882c9533ca7bc2e05d6854a885f8524
Kulraj Singh Kohli <kulraj0000@gmail.com>
2021-08-05 20:51:45 -0700

    Update ingress.py
    Finished adding one record to postgres

--- Inserting data/ingress.py
+++ Inserting data/ingress.py
@@ -16,9 +16,21 @@ cur = conn.cursor()
 
 # Create SQL query to insert data
 
-query = "insert into users (id, name, street, city, zip) values({},'{}','{}','{}','{}')".format(1,'Big Bird','Sesame Street','Fakeville','12345')
+query = "insert into people (id, name, street, city, zip) values({},'{}','{}','{}','{}')".format(1,'Big Bird','Sesame Street','Fakeville','12345')
 
 # Use mogrify method to see what you will be sending to the database
 
 cur.mogrify(query)
 
+# NOTE: table name is case sensitive and needs to be in double quotes
+query2 = 'insert into "People" (name,id,street,city,zip) values (%s,%s,%s,%s,%s)'
+data = ('Big Bird',1,'Sesame Street','Fakeville','12345')
+
+cur.mogrify(query2,data)
+
+# Execute query to add to the database
+
+cur.execute(query2,data)
+
+# Query is not permanent unless you can commit it
+conn.commit()
